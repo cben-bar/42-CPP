5a2d7b1d4127bcb64716d096a992d9c731a2a06f
cben-bar <ana.kronik@hotmail.fr>
2022-08-29 10:55:13 +0200

    cpp00/ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        while (av[i])
+        {
+            j = 0;
+            while (av[i][j])
+            {
+                av[i][j] = std::toupper(av[i][j]);
+                std::cout << av[i][j];
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
