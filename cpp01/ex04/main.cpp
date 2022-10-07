50f353caab5f8493a525e95b3ffb3b0c38682217
Clementine Ben barek <cben-bar@z4r3p5.42lyon.fr>
2022-10-07 15:14:20 +0200

    beg cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,27 @@
 /*   By: cben-bar <cben-bar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:58:02 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/10/07 13:58:53 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/10/07 15:13:25 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "sed.hpp"
+#include <fstream>
+#include <iostream>
 
-int main(int ac, char av)
+int main(int ac, char **av)
 {
+	if (ac != 4)
+	{
+		std::cout << "This programm takes a filename, and 2 strings" << std::endl;
+		return (0);
+	}
+	else
+	{
+		std::cout << "av[0] = " << av[0] << std::endl;
+		std::cout << "av[1] = " << av[1] << std::endl;
+		std::cout << "av[2] = " << av[2] << std::endl;
+		std::cout << "av[3] = " << av[3] << std::endl;
+		
+	}
 	return (0);
 }
