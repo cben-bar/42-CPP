baacb6f4da7f2832dcca339cb07307c8a4af9c09
Clementine Ben barek <cben-bar@z3r4p5.42lyon.fr>
2022-10-11 18:04:41 +0200

    main.c

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: cben-bar <cben-bar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:58:02 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/10/11 15:02:27 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/10/11 17:46:29 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
+#include <string>
 
 int main(int ac, char **av)
 {
@@ -27,6 +28,7 @@ int main(int ac, char **av)
 		std::cout << "av[2] = " << av[2] << std::endl;
 		std::cout << "av[3] = " << av[3] << std::endl;
 
+		std::ofstream	ofs("test.out");
 		std::ifstream	ifs(av[1]);
 		std::string		s1;
 		std::string		s2;
@@ -34,9 +36,10 @@ int main(int ac, char **av)
 		s1 = av[2];
 		s2 = av[3];
 
-		std::cout << s1 << " " << s2 << std::endl;
-
-		std::ofstream	ofs("test.out");
+		if (s1.compare(av[1]) == 0)
+		{
+			std::cout << s1 << " " << s2 << std::endl;
+		}
 		ofs << av[2] << std::endl;
 		ofs.close();
 	}
