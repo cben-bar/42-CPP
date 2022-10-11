f8c6796bc2190f688b2d5d02648c93a7da26c4b4
Clementine Ben barek <cben-bar@z3r4p5.42lyon.fr>
2022-10-11 15:18:11 +0200

    main.c

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:58:02 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/10/07 15:13:25 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/10/11 15:02:27 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,19 @@ int main(int ac, char **av)
 		std::cout << "av[1] = " << av[1] << std::endl;
 		std::cout << "av[2] = " << av[2] << std::endl;
 		std::cout << "av[3] = " << av[3] << std::endl;
-		
+
+		std::ifstream	ifs(av[1]);
+		std::string		s1;
+		std::string		s2;
+
+		s1 = av[2];
+		s2 = av[3];
+
+		std::cout << s1 << " " << s2 << std::endl;
+
+		std::ofstream	ofs("test.out");
+		ofs << av[2] << std::endl;
+		ofs.close();
 	}
 	return (0);
 }
