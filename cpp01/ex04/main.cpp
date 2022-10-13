79b38f460950d47713a97de3b8437c9ff462adac
Clementine Ben barek <cben-bar@z4r3p7.42lyon.fr>
2022-10-13 14:06:09 +0200

    cpp/ex04/main.cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:58:02 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/10/11 17:46:29 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 14:03:03 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,38 @@
 #include <iostream>
 #include <string>
 
+
+int	sed(std::string filename, std::string s1, std::string s2)
+{
+		std::ifstream	ifs_read(filename);
+		std::string		line;
+		std::size_t		found_i;
+
+		if (ifs_read.fail())
+		{
+			std::cout << "Error : read access denied" << std::endl;
+			ifs_read.close();
+			return (1);
+		}
+		std::ofstream	ofs_write(filename + ".replace");
+		while (std::getline(ifs_read, line))
+		{
+			found_i = line.find(s1);
+			while (found_i != std::string::npos)
+			{
+				if (!found_i && !s1.size())
+					break;
+				line.erase(found_i, s1.length());
+				line.insert(found_i, s2);
+				found_i += s2.size();
+				found_i = line.find(s1, found_i);
+			}
+			ofs_write << line << std::endl;
+		}
+		ifs_read.close();
+		ofs_write.close();
+		return (0);
+}
 int main(int ac, char **av)
 {
 	if (ac != 4)
@@ -22,26 +54,6 @@ int main(int ac, char **av)
 		return (0);
 	}
 	else
-	{
-		std::cout << "av[0] = " << av[0] << std::endl;
-		std::cout << "av[1] = " << av[1] << std::endl;
-		std::cout << "av[2] = " << av[2] << std::endl;
-		std::cout << "av[3] = " << av[3] << std::endl;
-
-		std::ofstream	ofs("test.out");
-		std::ifstream	ifs(av[1]);
-		std::string		s1;
-		std::string		s2;
-
-		s1 = av[2];
-		s2 = av[3];
-
-		if (s1.compare(av[1]) == 0)
-		{
-			std::cout << s1 << " " << s2 << std::endl;
-		}
-		ofs << av[2] << std::endl;
-		ofs.close();
-	}
+		sed(av[1], av[2], av[3]);
 	return (0);
 }
