591454ba88bd6d342fc51b657847525764457d6f
Clementine Ben barek <cben-bar@z4r3p7.42lyon.fr>
2022-10-13 14:50:35 +0200

    -

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,13 +6,24 @@
 /*   By: cben-bar <cben-bar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 14:32:04 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/10/13 14:32:29 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 14:48:37 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-	
+
+	if (ac != 2)
+	{
+		std::cout << "This programm takes an argument: DEBUG, INFO, WARNING or ERROR" << std::endl;
+		return (0);
+	}
+	else
+	{
+		std::cout << "av[0] =" << av[0] << std::endl;
+		std::cout << "av[1] =" << av[1] << std::endl;
+	}
+	return (0);
 }
