2a63447584b8973d9650ffc1603e96f8679c2a48
cben-bar <ana.kronik@hotmail.fr>
2023-02-04 22:42:14 +0100

    cpp05/ex01 check public attribut (exception)

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: beni <beni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 16:07:13 by beni              #+#    #+#             */
-/*   Updated: 2023/01/27 20:12:12 by beni             ###   ########.fr       */
+/*   Updated: 2023/02/04 22:19:53 by beni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,23 @@ class Form
         int                 getGradeToExec() const;
         void                beSigned(Bureaucrat &bureaucrat);
 
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowExcpetion : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
     private:
         const std::string   _name;
         bool                _signed;
         int                 _grade_to_sign;
         int                 _grade_to_execute;
     
-    class GradeTooHighException : public std::exception
-    {
-        public:
-            virtual const char* what() const throw();
-    };
-    class GradeTooLowExcpetion : public std::exception
-    {
-        public:
-            virtual const char* what() const throw();
-    };
 };
 
 std::ostream &operator<<(std::ostream &o, const Form &rhs);
