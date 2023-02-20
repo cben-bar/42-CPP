3566d8c0ae7e86abee31b7ab01d45b4d34f455c7
Clementine Ben barek <cben-bar@z2r10p4.42lyon.fr>
2023-02-20 17:18:11 +0100

    -_-

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 17:56:39 by cben-bar          #+#    #+#             */
-/*   Updated: 2023/02/19 19:20:41 by cben-bar         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:22:23 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,39 @@
 #include <iostream>
 #include <time.h>
 #include <cstdlib>
+
+///////////////////////////////////////
+//*/ */ */ */    COLORS   /* /* /* /*//
+///////////////////////////////////////
+
+#define WHITE           "\x1B[37m"
+#define GREY            "\x1B[30m"
+
+#define BLAND_BLUE       "\x1b[38;5;4m"
+#define STORM_BLUE     "\x1b[38;5;12m"
+#define CYAN            "\x1B[36m"
+
+#define GREEN           "\x1B[32m"
+#define SEA_GREEN       "\x1b[38;5;6m"
+#define PASTEL_GREEN    "\x1b[38;5;159m"
+#define FLUO_GREEN      "\x1b[38;5;46m"
+#define POOL_GREEN      "\x1b[38;5;50m"
+
+#define RED             "\x1B[31m"
+#define BLOOD_RED       "\x1b[38;5;125m"
+
+#define YELLOW          "\x1B[33m"
+#define PASTEL_YELLOW   "\x1b[38;5;229m"
+#define SUN             "\x1b[38;5;222m"
+#define TAN             "\x1b[38;5;172m"
+
+#define MAGENTA         "\x1b[35m"
+#define PASTEL_PINK     "\x1b[38;5;213m"
+#define PASTEL_PURPLE   "\x1b[38;5;147m"
+#define PARMA           "\x1b[38;5;105m"
+#define PURPLE          "\x1b[38;5;140m"
+#define VIOLET          "\x1b[38;5;129m"
+#define POWDERY_PINK    "\x1b[38;5;225m"
 
 class Base
 {
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:03:10 by cben-bar          #+#    #+#             */
-/*   Updated: 2023/02/19 19:09:53 by cben-bar         ###   ########.fr       */
+/*   Updated: 2023/02/20 16:08:01 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,91 @@
 #include "B.hpp"
 #include "C.hpp"
 
+Base * generate(void)
+{
+	srand (time(NULL));
+	int	random = std::rand() % 3;
+	std::cout << PURPLE << "\t    RANDOM INSTANTIATION" << std::endl;
+	switch (random)
+	{
+		case 1:
+		{
+			std::cout << POOL_GREEN << "Generate: A. --OK--" << std::endl;
+			return (new(A));
+		}
+		case 2:
+		{
+			std::cout << PASTEL_YELLOW << "Generate: B. --OK--" << std::endl;
+			return (new(B));
+		}
+		default:
+		{
+			std::cout << MAGENTA << "Generate: C. --OK--" << std::endl;
+			return (new(C));
+		}
+	}
+	return (NULL);
+}
+
+void identify(Base* p)
+{
+	A	*a;
+	B	*b;
+	C	*c;
+
+	a = dynamic_cast<A*>(p);
+	b = dynamic_cast<B*>(p);
+	c = dynamic_cast<C*>(p);
+	std::cout << PURPLE << std::endl << "\t\tIDENTIFY PTR" << std::endl;
+	if (a)
+		std::cout << POOL_GREEN << "Actual type of the object pointed to by p: A" << std::endl;
+	else if (b)
+		std::cout << PASTEL_YELLOW << "Actual type of the object pointed to by p: B" << std::endl;
+	else if (c)
+		std::cout << MAGENTA << "Actual type of the object pointed to by p: C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	std::cout << PURPLE << std::endl << "\t\t IDENTIFY &" << std::endl;
+	try
+	{
+		A	&rA = dynamic_cast<A&>(p);
+		std::cout << POOL_GREEN << "Actual type of the object pointed to by p: A" << std::endl << std::endl;
+		(void)rA;
+		return;
+	}
+	catch(const std::exception &e)
+	{}
+
+	try
+	{
+		B	&rB = dynamic_cast<B&>(p);
+		std::cout << PASTEL_YELLOW << "Actual type of the object pointed to by p: B" << std::endl << std::endl;
+		(void)rB;
+		return;
+	}
+	catch(const std::exception &e)
+	{}
+
+	try
+	{
+		C	&rC = dynamic_cast<C&>(p);
+		std::cout << MAGENTA << "Actual type of the object pointed to by p: C" << std::endl << std::endl;
+		(void)rC;
+		return;
+	}
+	catch(const std::exception &e)
+	{}
+}
+
 int main()
 {
+	Base	*randomBase = generate();
+
+	identify(randomBase);
+	identify(*randomBase);
+	delete randomBase;
+	std::cout << WHITE;
 	return (0);
 }
