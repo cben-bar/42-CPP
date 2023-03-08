10988d1ac3583265e76468fa33bb00ee8cde1787
Clementine Ben barek <cben-bar@z2r4p3.42lyon.fr>
2023-03-08 15:28:36 +0100

    -_-

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:03:10 by cben-bar          #+#    #+#             */
-/*   Updated: 2023/03/07 15:29:05 by cben-bar         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:27:33 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ int main()
 	std::cout << PASTEL_PINK << "Value of data.b: " << data.b << std::endl << std::endl;
 
 	ser = serialize(&data);
-	std::cout << PASTEL_GREEN << "\tNEW SER MEMORY ADDRESS: " << std::endl << std::endl;
-	std::cout << POOL_GREEN << "====> memory address: " << &ser << std::endl << std::endl;
+	std::cout << PASTEL_GREEN << "\tSER MEMORY ADDRESS: " << std::endl << std::endl;
+	std::cout << POOL_GREEN << "====> ser: " << std::hex << ser << std::endl << std::endl;
 
 	des = deserialize(ser);
-	std::cout << PASTEL_GREEN << "\tNEW DES MEMORY ADDRESS: " << std::endl << std::endl;
+	std::cout << PASTEL_GREEN << "\tDES MEMORY ADDRESS: " << std::endl << std::endl;
 	std::cout << POOL_GREEN << "====> memory address: " << des << std::endl << std::endl;
 	std::cout << POOL_GREEN << "Value of des->a: " << des->a << std::endl;
 	std::cout << POOL_GREEN << "Value of des->b: " << des->b << std::endl;
--- cpp07/ex02/Array.tpp
+++ cpp07/ex02/Array.tpp
@@ -6,7 +6,7 @@
 /*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 18:24:23 by cben-bar          #+#    #+#             */
-/*   Updated: 2023/03/07 18:24:35 by cben-bar         ###   ########.fr       */
+/*   Updated: 2023/03/08 14:50:46 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,39 @@
 
 template<typename T>
 Array<T>::Array(): _size(0), _cel(NULL)
+{
+	return;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n): _size(n), _cel(new T[n])
+{
+	return;
+}
+
+template<typename T>
+Array<T>::Array(const Array &origin): _size(origin._size), _cel(new T[origin._size])
+{
+	for (unsigned int n; n < origin._size; n++)
+		_cel[n] = origin._cel[n];
+}
+
+///////////////////////////////////////
+//*/ */ */ */  ASSIGNMENT /* /* /* /*//
+///////////////////////////////////////
+
+Array<T> &Array<T>::operator=(const Array &origin)
+{
+	if (this == &origin)
+		return (*this);
+	
+}
+
+///////////////////////////////////////
+//*/ */ */ */  DESTRUCTOR /* /* /* /*//
+///////////////////////////////////////
+
+Array<T>::~Array()
 {
 	return;
 }
