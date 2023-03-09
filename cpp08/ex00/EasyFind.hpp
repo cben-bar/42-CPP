8238453cce082ce67637a59affef49c4416a0094
Clementine Ben barek <cben-bar@z1r10p1.42lyon.fr>
2023-03-09 16:22:12 +0100

    cpp08/ex00

--- cpp08/ex00/EasyFind.hpp
+++ cpp08/ex00/EasyFind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   EasyFind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beni <beni@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:13:59 by beni              #+#    #+#             */
-/*   Updated: 2023/03/09 13:22:18 by beni             ###   ########.fr       */
+/*   Updated: 2023/03/09 16:21:06 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,68 @@
 
 #include <iostream>
 #include <list>
+#include <vector>
+#include <exception>
+#include <algorithm>
 
-class noOccurenceIsFound : public std::exception
+///////////////////////////////////////
+//*/ */ */ */    COLORS   /* /* /* /*//
+///////////////////////////////////////
+
+#define WHITE           "\x1B[37m"
+#define GREY            "\x1B[30m"
+
+#define BLAND_BLUE       "\x1b[38;5;4m"
+#define STORM_BLUE     "\x1b[38;5;12m"
+#define CYAN            "\x1B[36m"
+
+#define GREEN           "\x1B[32m"
+#define SEA_GREEN       "\x1b[38;5;6m"
+#define PASTEL_GREEN    "\x1b[38;5;159m"
+#define FLUO_GREEN      "\x1b[38;5;46m"
+#define POOL_GREEN      "\x1b[38;5;50m"
+
+#define RED             "\x1B[31m"
+#define BLOOD_RED       "\x1b[38;5;125m"
+
+#define YELLOW          "\x1B[33m"
+#define PASTEL_YELLOW   "\x1b[38;5;229m"
+#define SUN             "\x1b[38;5;222m"
+#define TAN             "\x1b[38;5;172m"
+
+#define MAGENTA         "\x1b[35m"
+#define PASTEL_PINK     "\x1b[38;5;213m"
+#define PASTEL_PURPLE   "\x1b[38;5;147m"
+#define PARMA           "\x1b[38;5;105m"
+#define PURPLE          "\x1b[38;5;140m"
+#define VIOLET          "\x1b[38;5;129m"
+#define POWDERY_PINK    "\x1b[38;5;225m"
+
+///////////////////////////////////////
+//*/ */ */ */ EXCEPTIONS  /* /* /* /*//
+///////////////////////////////////////
+
+class NoOccurenceIsFound : public std::exception
 {
-    virtual const char * what() const throw();
+    public:
+        virtual const char* what() const throw() {return ("Exception: No occurrence is found.");}
 };
 
-const char *noOccurrenceIsFound::what() const trhow()
-{
-    return ("Exception: No occurrence is found.");
-}
+///////////////////////////////////////
+//*/ */ */ */   TEMPLATE  /* /* /* /*//
+///////////////////////////////////////
 
 template<typename T>
-void    easyfind(T container, int n)
+void    easyfind(T c, int n)
 {
-    
+    typename T::iterator i;
+    i = find(c.begin(), c.end(), n);
+    if (i == c.end())
+        throw NoOccurenceIsFound();
+    else
+    {
+        std::cout << FLUO_GREEN << "Occurrence " << n << " in the first parameter has been found." << std::endl;
+    }
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beni <beni@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cben-bar <cben-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:22:26 by beni              #+#    #+#             */
-/*   Updated: 2023/03/09 13:23:01 by beni             ###   ########.fr       */
+/*   Updated: 2023/03/09 16:21:32 by cben-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,74 @@
 
 int main()
 {
-
+    std::cout << PARMA << "\t\tLST TEST" << std::endl << std::endl;
+    
+    std::list<int>  lstInt;
+    lstInt.push_back(1);
+    lstInt.push_back(2);
+    lstInt.push_back(3);
+    lstInt.push_back(4);
+    lstInt.push_back(5);
+    lstInt.push_back(6);
+    try
+    {
+        easyfind(lstInt, 5);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << BLOOD_RED << e.what() << std::endl;
+    }
+    try
+    {
+        easyfind(lstInt, 10);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << BLOOD_RED << e.what() << std::endl;
+    }
+    try
+    {
+        easyfind(lstInt, 6);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << BLOOD_RED << e.what() << std::endl;
+    }
+    
+    std::cout << std::endl << PARMA << "\t\tVECTOR TEST INT" << std::endl << std::endl;
+    
+    std::vector<int>    vectorInt(6);
+    vectorInt[0] = 1;
+    vectorInt[1] = 2;
+    vectorInt[2] = 3;
+    vectorInt[3] = 4;
+    vectorInt[4] = 5;
+    vectorInt[5] = 6;
+    try
+    {
+        easyfind(vectorInt, 5);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << BLOOD_RED << e.what() << std::endl;
+    }
+    
+    std::cout << std::endl << PARMA << "\t\tVECTOR TEST CHAR" << std::endl << std::endl;
+    std::vector<char>    vectorChar(6);
+    vectorChar[0] = 'a';
+    vectorChar[1] = 'b';
+    vectorChar[2] = 'c';
+    vectorChar[3] = 'd';
+    vectorChar[4] = 'e';
+    vectorChar[5] = 'f';
+    try
+    {
+        easyfind(vectorChar, 97);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << BLOOD_RED << e.what() << std::endl;
+    }
+    std::cout << WHITE;
     return (0);
 }
